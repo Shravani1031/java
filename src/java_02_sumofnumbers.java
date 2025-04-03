953119a5f00b8ce5a3935dcf0a8cd59231d77e77
shruti <shrutikadam30032@gmail.com>
2025-04-04 00:24:33 +0530

    first commit

---
+++ src/java_02_sumofnumbers.java
@@ -0,0 +1,9 @@
+public class java_02_sumofnumbers {
+    public static void main(String[]args){
+        System.out.println("The sum of these numbers is");
+        int n1=6;
+        int n2=8;
+        int n3=5;
+        System.out.print(n1+n2+n3);
+    }
+}
